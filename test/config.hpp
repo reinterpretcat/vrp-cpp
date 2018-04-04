f04e1ac80bb4c96d2ded459f2f7e9264e1ca8337
reinterpretcat <ilya.builuk@gmail.com>
2018-04-04 21:43:19 +0200

    Remove deprecated constant from test config

--- test/config.hpp
+++ test/config.hpp
@@ -5,6 +5,5 @@
 
 #define SOLOMON_DATA_PATH RESOURCES_PATH "/data/solomon/"
 #define SOLOMON_BENCHMARKS_PATH RESOURCES_PATH "/data/solomon/benchmarks/"
-#define SOLOMON_TESTS_PATH RESOURCES_PATH "/data/solomon/tests/"
 
 #endif //VRP_CONFIG_HPP
