8f4a21f2f9aa5ffa1e73f9d4f7bf572cc8f1d3da
reinterpretcat <ilya.builuk@gmail.com>
2018-04-20 22:44:12 +0200

    Optimize total cost calculations to avoid extra allocations

--- src/algorithms/Costs.cu
+++ src/algorithms/Costs.cu
@@ -6,9 +6,11 @@
 #include "models/Tasks.hpp"
 #include "models/Transition.hpp"
 
-#include <thrust/unique.h>
+#include <thrust/device_malloc.h>
+#include <thrust/device_free.h>
 #include <thrust/iterator/discard_iterator.h>
 #include <thrust/iterator/transform_output_iterator.h>
+#include <thrust/unique.h>
 
 namespace vrp {
 namespace algorithms {
@@ -35,26 +37,36 @@ struct calculate_cost final {
 };
 
 /// Calculates cost of all used vehicles separately.
-struct calculate_vehicles_cost final {
+struct calculate_total_cost final {
 
+  /// Calculates total cost.
   struct RouteCost final {
-    template<class Tuple>
-    __host__ __device__
-    float operator()(const Tuple& tuple) const {
+
+    float *total;
+
+     template<class Tuple>
+    __device__
+    float operator()(const Tuple &tuple) {
       const int task = thrust::get<0>(tuple);
       const int vehicle = thrust::get<1>(tuple);
       const float cost = thrust::get<2>(tuple);
       // TODO calculate return to depot cost
-      return cost;
+
+      atomicAdd(total, cost);
+
+      // NOTE ignored
+      return 0;
     }
   };
 
-  thrust::device_vector<float> operator()(const vrp::models::Tasks &tasks, int solution = 0) const {
+  float operator()(const vrp::models::Tasks &tasks, int solution = 0) const {
     int rbegin = tasks.size() - tasks.customers * (solution + 1);
     int rend = rbegin + tasks.customers;
     auto count = static_cast<std::size_t >(tasks.vehicles.back() + 1);
 
-    thrust::device_vector<float> costs(count);
+    // NOTE to use atomicAdd, variable has to be allocated in memory.
+    thrust::device_ptr<float> total = thrust::device_malloc<float>(1);
+    *total = 0;
 
     thrust::unique_by_key_copy(
         thrust::device,
@@ -66,10 +78,16 @@ struct calculate_vehicles_cost final {
             tasks.costs.rbegin() + rbegin)
         ),
         thrust::make_discard_iterator(),
-        thrust::make_transform_output_iterator(costs.rbegin(), RouteCost())
+        thrust::make_transform_output_iterator(
+            thrust::make_discard_iterator(),
+            RouteCost { total.get() }
+        )
     );
 
-    return std::move(costs);
+    float result = *total;
+    thrust::device_free(total);
+
+    return result;
   }
 };
 
--- test/algorithms/CostsTest.cu
+++ test/algorithms/CostsTest.cu
@@ -10,7 +10,7 @@
 using namespace vrp::algorithms;
 using namespace vrp::test;
 
-SCENARIO("Can calculate vehicles cost from solution.", "[algorithm][costs]") {
+SCENARIO("Can calculate total cost from solution.", "[algorithm][costs]") {
   auto stream = SolomonBuilder()
       .setTitle("Exceeded capacity and two vehicles")
       .setVehicle(3, 10)
@@ -22,11 +22,9 @@ SCENARIO("Can calculate vehicles cost from solution.", "[algorithm][costs]") {
       .addCustomer({5, 5, 0, 3, 0, 1000, 0})
       .build();
 
-  auto costs = calculate_vehicles_cost()(createPopulation<>(stream, 1));
+  auto cost = calculate_total_cost()(createPopulation<>(stream, 1));
 
   // vehicles: 0, 0, 1, 2, 2, 2
   // costs   : 0, 1, 2, 3, 4, 5
-  CHECK_THAT(vrp::test::copy(costs), Catch::Matchers::Equals(std::vector<float>{
-      1, 2, 5
-  }));
+  REQUIRE(cost == 8);
 }
