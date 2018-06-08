778a2ba8ef0f722bde9fd1efa4a3c063dcd635df
Ilya Builuk <ilya.builuk@gmail.com>
2018-06-08 18:24:22 +0200

    Fix convolution tests

--- test/algorithms/convolutions/JointConvolutionsTest.cu
+++ test/algorithms/convolutions/JointConvolutionsTest.cu
@@ -3,6 +3,7 @@
 #include "test_utils/MemoryUtils.hpp"
 #include "test_utils/TaskUtils.hpp"
 #include "test_utils/VectorUtils.hpp"
+#include "utils/memory/Allocations.hpp"
 
 #include <catch/catch.hpp>
 #include <thrust/execution_policy.h>
@@ -13,44 +14,97 @@ using namespace vrp::models;
 using namespace vrp::utils;
 using namespace vrp::test;
 
-/*
 namespace {
-Convolutions map(const std::vector<Convolution> convolutions, Pool& pool) {
-  auto data = pool.acquire<thrust::device_vector<Convolution>>(convolutions.size());
-  thrust::copy(convolutions.begin(), convolutions.end(), data->begin());
-  return data;
-}
-}
+const int customers = 20 + 1;
 
-/*SCENARIO("Can create joint convolution pair from two convolutions", "[convolution][join_pairs]") {
-  int customers = 20 + 1;
+using JoinPairResult = thrust::pair<thrust::pair<size_t, size_t>, thrust::device_ptr<JointPair>>;
+using Result = thrust::pair<thrust::pair<size_t, size_t>, thrust::host_vector<JointPair>>;
+
+/// Minimal solution to launch testing function.
+Solution createBasicSolution() {
   auto problem = Problem();
   auto tasks = Tasks(customers, 2);
   tasks.ids = create({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
-  Pool pool;
+
+  return {std::move(problem), std::move(tasks)};
+}
+
+/// Prepares and runs testing function on device.
+struct run_joint_convolutions final {
+  using ConvolutionData = thrust::pair<size_t, thrust::device_ptr<Convolution>>;
+
+  Solution::Shadow solution;
+  DevicePool::Pointer pool;
+  Settings settings;
+  ConvolutionData left;
+  ConvolutionData right;
+
+  thrust::device_ptr<JoinPairResult> result;
+
+  __device__ void operator()(int index) {
+    auto leftPooled = pool.get()->convolutions(static_cast<size_t>(customers));
+    thrust::copy(thrust::device, left.second, left.second + left.first, *leftPooled);
+    auto rightPooled = pool.get()->convolutions(static_cast<size_t>(customers));
+    thrust::copy(thrust::device, right.second, right.second + right.first, *rightPooled);
+
+    auto joinPairs = create_joint_convolutions{solution, pool}(
+      settings, {left.first, std::move(leftPooled)}, {right.first, std::move(rightPooled)});
+
+    thrust::copy(thrust::device, *joinPairs.data,
+                 *joinPairs.data + joinPairs.dimens.first * joinPairs.dimens.second,
+                 result.get()->second);
+
+    result.get()->first = joinPairs.dimens;
+  };
+};
+
+/// Calculates results with handling memory transfer between host and device
+Result getResult(Solution& solution,
+                 const std::vector<Convolution>& left,
+                 const std::vector<Convolution>& right) {
+  const int expected = 6;
+  thrust::device_vector<JointPair> resultData(expected);
+  thrust::device_vector<Convolution> leftDev(left.begin(), left.end());
+  thrust::device_vector<Convolution> rightDev(right.begin(), right.end());
+
+  auto runner = run_joint_convolutions{solution.getShadow(),
+                                       getPool(),
+                                       {0.75, 0.1},
+                                       {leftDev.size(), leftDev.data()},
+                                       {rightDev.size(), rightDev.data()},
+                                       allocate<JoinPairResult>({{0, 0}, resultData.data()})};
+  thrust::for_each(thrust::device, thrust::make_counting_iterator(0),
+                   thrust::make_counting_iterator(1), runner);
+  auto result = release(runner.result);
+  thrust::host_vector<JointPair> data(result.second, result.second + expected);
+
+  return {result.first, std::move(data)};
+}
+
+}  // namespace
+
+SCENARIO("Can create joint convolution pair from two convolutions", "[convolution][join_pairs]") {
+  Solution solution = createBasicSolution();
   std::vector<Convolution> left = {{0, 1, 1, {1, 4}, {}, {1, 4}},
                                    {0, 2, 2, {7, 13}, {}, {7, 13}},
                                    {0, 3, 3, {15, 19}, {}, {15, 19}}};
-  std::vector<Convolution> right{{customers, 4, 4, {1, 6}, {}, {1, 6}},
-                                 {customers, 5, 5, {6, 11}, {}, {6, 11}}};
-  Solution solution(std::move(problem), std::move(tasks));
+  std::vector<Convolution> right = {{customers, 4, 4, {1, 6}, {}, {1, 6}},
+                                    {customers, 5, 5, {6, 11}, {}, {6, 11}}};
 
 
-  auto result =
-    create_joint_convolutions{}(solution, {0.2, 0.2, pool}, map(left, pool), map(right, pool));
+  auto result = getResult(solution, left, right);
 
 
-  REQUIRE(result.dimens.first == 3);
-  REQUIRE(result.dimens.second == 2);
+  REQUIRE(result.first.first == 3);
+  REQUIRE(result.first.second == 2);
 
-  compare(result.pairs->operator[](0), {4, 6, {left.at(0), right.at(0)}});
-  compare(result.pairs->operator[](1), {0, 10, {left.at(0), right.at(1)}});
+  compare(result.second[0], {4, 6, {left.at(0), right.at(0)}});
+  compare(result.second[1], {0, 10, {left.at(0), right.at(1)}});
 
-  compare(result.pairs->operator[](2), {0, 13, {left.at(1), right.at(0)}});
-  compare(result.pairs->operator[](3), {5, 8, {left.at(1), right.at(1)}});
+  compare(result.second[2], {0, 13, {left.at(1), right.at(0)}});
+  compare(result.second[3], {5, 8, {left.at(1), right.at(1)}});
 
-  compare(result.pairs->operator[](4), {0, 11, {left.at(2), right.at(0)}});
-  compare(result.pairs->operator[](5), {0, 11, {left.at(2), right.at(1)}});
+  compare(result.second[4], {0, 11, {left.at(2), right.at(0)}});
+  compare(result.second[5], {0, 11, {left.at(2), right.at(1)}});
 }
-*/
--- test/algorithms/convolutions/SlicedConvolutionsTest.cu
+++ test/algorithms/convolutions/SlicedConvolutionsTest.cu
@@ -1,6 +1,7 @@
 #include "algorithms/convolutions/BestConvolutions.hpp"
 #include "algorithms/convolutions/JointConvolutions.hpp"
 #include "algorithms/convolutions/SlicedConvolutions.hpp"
+#include "test_utils/ConvolutionUtils.hpp"
 #include "test_utils/MemoryUtils.hpp"
 #include "test_utils/TaskUtils.hpp"
 #include "test_utils/VectorUtils.hpp"
@@ -8,51 +9,95 @@
 #include <catch/catch.hpp>
 #include <thrust/execution_policy.h>
 #include <thrust/transform_reduce.h>
+#include <utils/memory/Allocations.hpp>
 
 using namespace vrp::algorithms::convolutions;
 using namespace vrp::models;
 using namespace vrp::utils;
 using namespace vrp::test;
 
-/*namespace {
-JointPairs createPairs(Pool& pool,
-                       const thrust::pair<size_t, size_t>& dimens,
-                       const std::initializer_list<JointPair>& list) {
-  auto pairs = pool.acquire<thrust::device_vector<JointPair>>(list.size());
-  thrust::copy(thrust::device, list.begin(), list.end(), pairs->begin());
-  return {dimens, std::move(pairs)};
-}
-};  // namespace
+namespace {
+const int customers = 20 + 1;
 
-SCENARIO("Can create sliced convolutions from joint pairs", "[convolution][sliced]") {
-  Pool pool;
-  int customers = 20 + 1;
+using ConvolutionResult = thrust::pair<size_t, thrust::device_ptr<Convolution>>;
+
+/// Creates solution skeleton.
+Solution createBasicSolution() {
   auto problem = Problem();
   auto tasks = Tasks(customers, 2);
-  auto settings = Settings{0.2, 0.2, pool};
   tasks.ids = create({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
+
+  return {std::move(problem), std::move(tasks)};
+}
+
+/// Prepares and runs testing function on device.
+struct run_sliced_convolutions final {
+  Solution::Shadow solution;
+  DevicePool::Pointer pool;
+  Settings settings;
+  thrust::pair<size_t, size_t> dimens;
+  thrust::device_ptr<JointPair> data;
+
+  thrust::device_ptr<ConvolutionResult> result;
+
+  __device__ void operator()(int index) {
+    auto jointPairs = pool.get()->jointPairs(customers);
+    thrust::copy(thrust::device, data, data + dimens.first * dimens.second, *jointPairs);
+
+    auto sliced =
+      create_sliced_convolutions{solution, pool}({0.2, 0.2}, {dimens, std::move(jointPairs)});
+
+    thrust::copy(thrust::device, *sliced.data, *sliced.data + sliced.size, result.get()->second);
+    result.get()->first = sliced.size;
+  }
+};
+
+/// Calculates results with handling memory transfer between host and device
+thrust::host_vector<Convolution> getResult(Solution& solution,
+                                           const thrust::pair<size_t, size_t>& dimens,
+                                           const std::initializer_list<JointPair>& list) {
+  const int expected = 5;
+  thrust::device_vector<Convolution> resultData(expected);
+  thrust::device_vector<JointPair> jointPairs(list.begin(), list.end());
+
+  auto runner = run_sliced_convolutions{
+    solution.getShadow(), getPool(),
+    {0.2, 0.2},           dimens,
+    jointPairs.data(),    allocate<ConvolutionResult>({0, resultData.data()})};
+
+  thrust::for_each(thrust::device, thrust::make_counting_iterator(0),
+                   thrust::make_counting_iterator(1), runner);
+
+  auto result = release(runner.result);
+  thrust::host_vector<Convolution> data(result.second, result.second + result.first);
+
+  return std::move(data);
+}
+
+}  // namespace
+
+SCENARIO("Can create sliced convolutions from joint pairs", "[convolution][sliced]") {
+  auto solution = createBasicSolution();
 
   std::vector<Convolution> left = {{0, 1, 1, {1, 4}, {}, {1, 4}},
                                    {0, 2, 2, {7, 13}, {}, {7, 13}},
                                    {0, 3, 3, {15, 19}, {}, {15, 19}}};
   std::vector<Convolution> right{{customers, 4, 4, {1, 6}, {}, {1, 6}},
                                  {customers, 5, 5, {6, 11}, {}, {6, 11}}};
-  auto pairs = createPairs(pool, {3, 2},
-                           {{4, 6, {left.at(0), right.at(0)}},
-                            {0, 10, {left.at(0), right.at(1)}},
-                            {0, 13, {left.at(1), right.at(0)}},
-                            {5, 8, {left.at(1), right.at(1)}},
-                            {0, 11, {left.at(2), right.at(0)}},
-                            {0, 11, {left.at(2), right.at(1)}}});
-  Solution solution(std::move(problem), std::move(tasks));
 
-  auto result = create_sliced_convolutions{}(solution, settings, pairs);
+  auto result = getResult(solution, {3, 2},
+                          {{4, 6, {left.at(0), right.at(0)}},
+                           {0, 10, {left.at(0), right.at(1)}},
+                           {0, 13, {left.at(1), right.at(0)}},
+                           {5, 8, {left.at(1), right.at(1)}},
+                           {0, 11, {left.at(2), right.at(0)}},
+                           {0, 11, {left.at(2), right.at(1)}}});
 
-  REQUIRE(result->size() == (left.size() + right.size()));
-  compare(result->operator[](0), left.at(0));
-  compare(result->operator[](1), right.at(1));
-  compare(result->operator[](2), left.at(1));
-  compare(result->operator[](3), right.at(0));
-  compare(result->operator[](4), left.at(2));
-}*/
+  REQUIRE(result.size() == 5);
+  compare(result[0], left.at(0));
+  compare(result[1], right.at(1));
+  compare(result[2], left.at(1));
+  compare(result[3], right.at(0));
+  compare(result[4], left.at(2));
+}
