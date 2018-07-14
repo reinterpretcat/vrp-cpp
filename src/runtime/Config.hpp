7d7b8361cfd3cabaef91b9359392eb1e62ca34cc
reinterpretcat <ilya.builuk@gmail.com>
2018-07-14 23:04:08 +0200

    TechDebt: Log execution unit with pragma message

--- src/runtime/Config.hpp
+++ src/runtime/Config.hpp
@@ -1,10 +1,11 @@
 #ifndef VRP_RUNTIME_CONFIG_HPP
 #define VRP_RUNTIME_CONFIG_HPP
 
-/// Configuration to run on device.
 #ifdef RUN_ON_DEVICE
+#pragma message ( "Configured to run on device." )
 #include "runtime/detail/device/Config.inl"
 #else
+#pragma message ( "Configured to run on host." )
 #include "runtime/detail/host/Config.inl"
 #endif
 
