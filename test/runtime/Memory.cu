445b8b46e6116c5c8c89fcd228265691df8107b3
reinterpretcat <ilya.builuk@gmail.com>
2018-07-23 15:31:55 +0200

    Move memory test to runtime

--- test/runtime/Memory.cu
+++ test/runtime/Memory.cu
@@ -5,14 +5,14 @@
 using namespace vrp::runtime;
 
 namespace {
-__global__ void useDeviceUniquePointer() {
+__global__ void useUniquePointer() {
   auto singleInt = make_unique_ptr_value<int>();
   auto arrayInt = make_unique_ptr_data<int>(5);
 }
 
 }  // namespace
 
-SCENARIO("Can use device unique pointer on device", "[utils][memory][device][pointer]") {
-  useDeviceUniquePointer<<<1, 1, 0>>>();
+SCENARIO("Can use unique pointer on device", "[utils][memory][device][pointer]") {
+  useUniquePointer<<<1, 1, 0>>>();
   cudaStreamSynchronize(0);
 }
