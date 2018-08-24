891ac7d6d8f2d668d2d65db07aff0806edd377f2
reinterpretcat <ilya.builuk@gmail.com>
2018-08-25 00:22:00 +0200

    Add test to reproduce mutation issue

--- test/algorithms/genetic/MutationTest.cu
+++ test/algorithms/genetic/MutationTest.cu
@@ -19,6 +19,9 @@ using namespace vrp::utils;
 
 namespace {
 
+const auto T = Plan::assign();
+const auto F = Plan::empty();
+
 /// Runs crossover
 struct run_mutation final {
   Solution::Shadow solution;
@@ -52,4 +55,41 @@ SCENARIO("Can mutate c101 individuum keeping convolutions.", "[genetic][mutation
 
 SCENARIO("Can mutate rc1_10_1 individuum keeping convolutions.", "[genetic][mutation][rc1_10_1]") {
   test<rc1_10_1_problem_stream, 1000>(Mutation{0, 1, {0.75, 3}});
+}
+
+SCENARIO("Can mutate specific individuum.", "[genetic][mutation][c101][specific]") {
+  auto solution = createPopulation(
+    createProblem<create_c101_problem_stream>(),
+
+    {0, 20, 21, 22, 23, 24, 25, 10, 11, 9, 6, 4, 5, 3, 7, 8, 2, 1, 13, 17, 18, 19, 15, 16, 14, 12,
+     0, 0,  0,  0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0},
+
+    {0,          10,         12,         12.1655254, 13,         15,         17,         34.2046509,
+     37.2046509, 40.3669281, 42.6029968, 44.8390656, 15.1327457, 16.1327457, 18.1327457, 20.9611721,
+     28.2412815, 30.2412815, 30.8058434, 34.8058434, 37.8058434, 42.8058434, 47.8058434, 52.8058434,
+     54.8058434, 57.8058434, 0,          0,          0,          0,          0,          0,
+     0,          0,          0,          0,          0,          0,          0,          0,
+     0,          0,          0,          0,          0,          0,          0,          0,
+     0,          0,          0,          0},
+
+    {0,   100, 1004, 902, 822, 155, 259, 447, 540, 633, 725, 817, 105, 196, 288, 380, 915, 1007,
+     120, 214, 307,  402, 497, 592, 684, 777, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0},
+
+    {200, 190, 170, 180, 190, 190, 150, 140, 130, 120, 100, 90, 190, 180, 160, 140, 110, 100,
+     170, 150, 130, 120, 80,  40,  30,  10,  200, 0,   0,   0,  0,   0,   0,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0},
+
+    {0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T,
+     T, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F});
+  auto mutation = Mutation{0, 1, {0.880393744, 2}};
+
+  thrust::for_each(exec_unit, thrust::make_counting_iterator(0), thrust::make_counting_iterator(1),
+                   run_mutation{solution.getShadow(), mutation});
+
+  MatrixTextWriter::write(std::cout, solution);
+  REQUIRE(SolutionChecker::check(solution).isValid());
 }
--- test/test_utils/PopulationFactory.hpp
+++ test/test_utils/PopulationFactory.hpp
@@ -11,6 +11,14 @@
 namespace vrp {
 namespace test {
 
+/// Creates problem.
+template<typename Problem>
+vrp::models::Problem createProblem() {
+  auto stream = Problem{}();
+  return vrp::streams::SolomonReader().read(stream,
+                                            vrp::algorithms::distances::cartesian_distance());
+}
+
 /// Creates population from input stream. Result includes problem definition.
 template<typename Heuristic = vrp::algorithms::heuristics::nearest_neighbor<
            vrp::algorithms::heuristics::TransitionOperator>>
@@ -18,6 +26,26 @@ vrp::models::Solution createPopulation(std::istream& stream, int populationSize 
   auto problem =
     vrp::streams::SolomonReader().read(stream, vrp::algorithms::distances::cartesian_distance());
   auto tasks = vrp::algorithms::genetic::create_population<Heuristic>{problem}(populationSize);
+  return vrp::models::Solution(std::move(problem), std::move(tasks));
+}
+
+/// Creates population from problem and task's data.
+inline vrp::models::Solution createPopulation(
+  vrp::models::Problem&& problem,
+  const std::initializer_list<int>& ids,
+  const std::initializer_list<float>& costs,
+  const std::initializer_list<int>& times,
+  const std::initializer_list<int>& capacities,
+  const std::initializer_list<int>& vehicles,
+  const std::initializer_list<vrp::models::Plan>& plan) {
+  auto tasks = vrp::models::Tasks();
+  tasks.customers = problem.size();
+  tasks.ids = std::vector<int>{ids};
+  tasks.costs = std::vector<float>{costs};
+  tasks.times = std::vector<int>{times};
+  tasks.capacities = std::vector<int>{capacities};
+  tasks.vehicles = std::vector<int>{vehicles};
+  tasks.plan = std::vector<vrp::models::Plan>{plan};
   return vrp::models::Solution(std::move(problem), std::move(tasks));
 }
 
