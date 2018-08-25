9456e99ee0b4270ff2326187833b9f1d8102f2e8
reinterpretcat <ilya.builuk@gmail.com>
2018-08-25 20:59:42 +0200

    Fix some of broken unit tests

--- test/algorithms/genetic/StrategiesTest.cu
+++ test/algorithms/genetic/StrategiesTest.cu
@@ -1,18 +1,26 @@
 #include "algorithms/genetic/Evolution.hpp"
 #include "algorithms/genetic/Models.hpp"
 #include "algorithms/genetic/Strategies.hpp"
+#include "algorithms/heuristics/NearestNeighbor.hpp"
+#include "test_utils/PopulationFactory.hpp"
+#include "test_utils/ProblemStreams.hpp"
 
 #include <catch/catch.hpp>
 #include <thrust/iterator/counting_iterator.h>
 
 using namespace vrp::algorithms::genetic;
+using namespace vrp::algorithms::heuristics;
 using namespace vrp::runtime;
 using namespace vrp::models;
+using namespace vrp::streams;
+using namespace vrp::test;
 
 SCENARIO("Can use linear strategy's selection and next", "[genetic][strategy]") {
+  auto stream = create_sequential_problem_stream{}();
+  auto solution = createPopulation<nearest_neighbor<TransitionOperator>>(stream, 2);
   auto strategy = LinearStrategy{{16}};
   auto ctx = EvolutionContext{0,
-                              {Problem(), Tasks()},
+                              std::move(solution),
                               vector<thrust::pair<int, float>>(static_cast<size_t>(16)),
                               thrust::minstd_rand()};
 
