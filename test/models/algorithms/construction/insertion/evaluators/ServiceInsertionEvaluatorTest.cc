62630205239ea669bd6100629c541a015ad08f98
reinterpretcat <ilya.builuk@gmail.com>
2018-11-17 00:37:46 +0100

    Add more work on service insertion test

--- test/models/algorithms/construction/insertion/evaluators/ServiceInsertionEvaluatorTest.cc
+++ test/models/algorithms/construction/insertion/evaluators/ServiceInsertionEvaluatorTest.cc
@@ -18,20 +18,18 @@ SCENARIO("service insertion evaluator", "[algorithms][construction][insertion]")
     auto constraint = std::make_shared<InsertionConstraint>();
     auto route = test_build_route{}.owned();
     auto evaluator = ServiceInsertionEvaluator(std::make_shared<TestTransportCosts>(),
-        std::make_shared<ActivityCosts>(),
-        constraint);
+                                               std::make_shared<ActivityCosts>(),
+                                               constraint);
 
     WHEN("service has failed constraint") {
-      constraint->addHardRoute([](const auto&, const auto&) { return InsertionConstraint::HardRouteResult{42}; });
+      constraint->addHardRoute([](const auto &, const auto &) { return InsertionConstraint::HardRouteResult{42}; });
       auto result = evaluator.evaluate(ranges::get<0>(DefaultService),
-                                       test_build_insertion_route_context{}.owned(), {});
+                                       test_build_insertion_route_context{}.owned(),
+                                       progress);
 
-      THEN("returns insertion failure") {
-         REQUIRE (result.index() == 1);
-      }
-
-      THEN("returns failed constraint code") {
-         REQUIRE (ranges::get<1>(result).constraint == 42);
+      THEN("returns insertion failure with proper code") {
+        REQUIRE (result.index() == 1);
+        REQUIRE (ranges::get<1>(result).constraint == 42);
       }
     }
 
@@ -42,17 +40,8 @@ SCENARIO("service insertion evaluator", "[algorithms][construction][insertion]")
 
       THEN("returns insertion success") {
         REQUIRE (result.index() == 0);
-      }
-
-      THEN("returns correct index") {
         REQUIRE (ranges::get<0>(result).index == 0);
-      }
-
-      THEN("returns correct departure time") {
         REQUIRE (ranges::get<0>(result).departure == 0);
-      }
-
-      THEN("returns correct activity location") {
         REQUIRE (ranges::get<0>(result).activity->location == DefaultJobLocation);
       }
     }
@@ -61,20 +50,39 @@ SCENARIO("service insertion evaluator", "[algorithms][construction][insertion]")
       auto service = test_build_service{}.details({{{}, DefaultDuration, {DefaultTimeWindow}}}).shared();
       auto result = evaluator.evaluate(service, test_build_insertion_route_context{}.owned(), progress);
 
-      THEN("returns insertion success") {
+      THEN("returns correct insertion success") {
         REQUIRE (result.index() == 0);
-      }
-
-      THEN("returns correct index") {
         REQUIRE (ranges::get<0>(result).index == 0);
-      }
-
-      THEN("returns correct departure time") {
         REQUIRE (ranges::get<0>(result).departure == 0);
-      }
-
-      THEN("returns correct activity location") {
         REQUIRE (ranges::get<0>(result).activity->location == 0);
+      }
+    }
+  }
+
+  GIVEN("tour with two activities") {
+    auto prev = test_build_activity{}.location(5).duration(0).schedule({5, 5}).shared();
+    auto next = test_build_activity{}.location(10).schedule({10, 10}).duration(0).shared();
+
+    auto constraint = std::make_shared<InsertionConstraint>();
+    auto progress = test_build_insertion_progress{}.owned();
+    auto routeCtx = test_build_insertion_route_context{}.shared();
+    routeCtx->route->tour.add(prev).add(next);
+
+    auto evaluator = ServiceInsertionEvaluator(std::make_shared<TestTransportCosts>(),
+                                               std::make_shared<ActivityCosts>(),
+                                               constraint);
+
+    auto[location, index] = GENERATE(std::make_tuple(8, 1));
+
+    WHEN("service is inserted") {
+      auto service = test_build_service{}.details({{{location}, 0, {DefaultTimeWindow}}}).shared();
+      auto result = evaluator.evaluate(service, *routeCtx, progress);
+
+      THEN("returns correct insertion success") {
+        REQUIRE (result.index() == 0);
+        REQUIRE (ranges::get<0>(result).index == index);
+        REQUIRE (ranges::get<0>(result).departure == 0);
+        REQUIRE (ranges::get<0>(result).activity->location == location);
       }
     }
   }
