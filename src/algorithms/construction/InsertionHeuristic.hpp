b614796b66824b47157b544a53d6456c23630229
reinterpretcat <ilya.builuk@gmail.com>
2018-11-29 20:51:24 +0100

    Finalize cheapest insertion

--- src/algorithms/construction/InsertionHeuristic.hpp
+++ src/algorithms/construction/InsertionHeuristic.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "algorithms/construction/InsertionContext.hpp"
+#include "algorithms/construction/InsertionResult.hpp"
 
 namespace vrp::algorithms::construction {
 
@@ -8,5 +9,24 @@ namespace vrp::algorithms::construction {
 template<typename Algorithm>
 struct InsertionHeuristic {
   InsertionContext insert(const InsertionContext& ctx) const { return static_cast<Algorithm*>(this)->analyze(ctx); }
+
+protected:
+  /// Inserts result into context.
+  void insert(const InsertionResult& result, InsertionContext& ctx) const {
+    result.visit(ranges::overload(
+      [&](const InsertionSuccess& success) {
+        // perform insertion
+        success.route.first->actor = success.actor;
+        ctx.registry->use(*success.actor);
+        // NOTE assume that activities are sorted by insertion index
+        ranges::for_each(success.activities | ranges::view::reverse,
+                         [&](const auto& act) { success.route.first->tour.insert(act.first, act.second); });
+        ctx.jobs.erase(success.job);
+      },
+      [&](const InsertionFailure& failure) {
+        ranges::for_each(ctx.jobs, [&](const auto& job) { ctx.unassigned[job] = failure.constraint; });
+        ctx.jobs.clear();
+      }));
+  }
 };
 }
--- src/algorithms/construction/heuristics/CheapestInsertion.hpp
+++ src/algorithms/construction/heuristics/CheapestInsertion.hpp
@@ -12,37 +12,20 @@ namespace vrp::algorithms::construction {
 
 /// Cheapest insertion heuristic.
 struct CheapestInsertion final : InsertionHeuristic<CheapestInsertion> {
-  CheapestInsertion(const InsertionEvaluator& evaluator) : evaluator_(evaluator) {}
+  explicit CheapestInsertion(const InsertionEvaluator& evaluator) : evaluator_(evaluator) {}
 
   InsertionContext analyze(const InsertionContext& ctx) const {
     auto newCtx = InsertionContext(ctx);
     while (!newCtx.jobs.empty()) {
       // TODO use C++17 parallel algorithms instead of rxcpp once it has better runtime support
-      rxcpp::observable<>::iterate(newCtx.jobs)
-        .map([&](const auto& job) { return evaluator_.evaluate(job, newCtx); })
-        .reduce(make_result_failure(),
-                [](const auto& acc, const auto& result) { return get_cheapest(acc, result); },
-                [](const auto& res) { return res; })
-        .as_blocking()
-        .last()
-        .visit(ranges::overload(
-          [&](const InsertionSuccess& success) {
-            // perform insertion
-            success.route.first->actor = success.actor;
-            ctx.registry->use(*success.actor);
-
-            ranges::for_each(success.activities, [&](const auto& act) {
-
-            });
-            newCtx.jobs.erase(success.job);
-          },
-          [&](const InsertionFailure& failure) {
-            //            ranges::push_back(newCtx.unassigned, newCtx.jobs | ranges::view::transform([&](const auto&
-            //            job) {
-            //                                                   return std::pair(job, failure.constraint);
-            //                                                 }));
-            newCtx.jobs.clear();
-          }));
+      insert(rxcpp::observable<>::iterate(newCtx.jobs)
+               .map([&](const auto& job) { return evaluator_.evaluate(job, newCtx); })
+               .reduce(make_result_failure(),
+                       [](const auto& acc, const auto& result) { return get_cheapest(acc, result); },
+                       [](const auto& res) { return res; })
+               .as_blocking()
+               .last(),
+             newCtx);
     }
     return newCtx;
   }
