95779ac5f95d4235f44d19f8bd618ab480cd14b4
reinterpretcat <ilya.builuk@gmail.com>
2018-12-09 18:15:22 +0100

    Adjust code style

--- test/models/problem/FleetTest.cc
+++ test/models/problem/FleetTest.cc
@@ -1,7 +1,8 @@
 #include "models/problem/Fleet.hpp"
 
-#include <catch/catch.hpp>
 #include "test_utils/models/Factories.hpp"
+
+#include <catch/catch.hpp>
 
 using namespace vrp::models::problem;
 using namespace Catch::Matchers;
@@ -11,18 +12,15 @@ namespace vrp::test {
 SCENARIO("fleet model", "[models][problem][fleet]") {
   GIVEN("fleet with different vehicles") {
     auto fleet = Fleet{}
-        .add(test_build_vehicle{}.id("v1").profile("1").details({{0, 0, {0, 100}}}).owned())
-        .add(test_build_vehicle{}.id("v2").profile("2").details({{0, 0, {0, 100}}}).owned())
-        .add(test_build_vehicle{}.id("v3").profile("1").details({{0, 0, {0, 100}}}).owned());
+                   .add(test_build_vehicle{}.id("v1").profile("1").details({{0, 0, {0, 100}}}).owned())
+                   .add(test_build_vehicle{}.id("v2").profile("2").details({{0, 0, {0, 100}}}).owned())
+                   .add(test_build_vehicle{}.id("v3").profile("1").details({{0, 0, {0, 100}}}).owned());
 
     WHEN("get profiles") {
       auto profiles = fleet.profiles() | ranges::to_vector;
 
-      THEN("return unique profiles") {
-        CHECK_THAT(profiles, Equals(std::vector<std::string>{"1", "2"}));
-      }
+      THEN("return unique profiles") { CHECK_THAT(profiles, Equals(std::vector<std::string>{"1", "2"})); }
     }
   }
 }
-
 }
