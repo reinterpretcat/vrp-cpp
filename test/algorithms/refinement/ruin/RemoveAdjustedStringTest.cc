b7c187098908dcc6de7606760f82c04e439bac73
reinterpretcat <ilya.builuk@gmail.com>
2018-12-30 21:54:32 +0100

    Add more test cases for RAS

--- test/algorithms/refinement/ruin/RemoveAdjustedStringTest.cc
+++ test/algorithms/refinement/ruin/RemoveAdjustedStringTest.cc
@@ -15,12 +15,13 @@ namespace {
 /// Uses predefined values to control algorithm execution.
 /// int distribution values:
 /// 1. route index in solution
-/// 2. job index in selected route tour
-/// 3. selected algorithm: 1: sequential algorithm
-/// 4. string removal index
+/// 2*. job index in selected route tour
+/// 3*. selected algorithm: 1: sequential algorithm
+/// 4*. string removal index(-ies)
 /// double distribution values:
 /// 1. string count
-/// 2. string size
+/// 2*. string size(-s)
+/// (*) - specific for each route.
 template<typename T>
 struct FakeDistribution {
   std::vector<T> values = {};
@@ -37,10 +38,10 @@ struct FakeDistribution {
 namespace vrp::test {
 
 SCENARIO("adjusted string removal can ruin solution", "[algorithms][refinement][ruin]") {
-  auto ras = RemoveAdjustedString{};
-
   auto [ints, doubles, ids] = GENERATE(table<std::vector<int>, std::vector<double>, std::vector<std::string>>({
     {{1, 2, 1, 1}, {1, 3}, {"c6", "c7", "c8"}},
+    {{0, 2, 1, 1, 1, 1, 2}, {2, 3, 2}, {"c1", "c2", "c3", "c7", "c8"}},
+    {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, {3, 3, 3, 3}, {"c1", "c11", "c12", "c13", "c2", "c3", "c6", "c7", "c8"}},
   }));
 
   GIVEN("solution with 3 routes within 5 service jobs in each") {
@@ -51,7 +52,8 @@ SCENARIO("adjusted string removal can ruin solution", "[algorithms][refinement][
         problem,
         std::make_shared<Random>(FakeDistribution<int>{ints, 0}, FakeDistribution<double>{doubles, 0}),
         std::make_shared<std::set<Job, compare_jobs>>()};
-      ras.operator()(context, *solution);
+
+      RemoveAdjustedString{}.operator()(context, *solution);
 
       THEN("should ruin expected jobs") {
         CHECK_THAT(get_job_ids_from_set{}.operator()(solution->unassigned), Equals(ids));
