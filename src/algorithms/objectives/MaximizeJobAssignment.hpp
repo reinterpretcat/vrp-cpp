069590675a17f268ce49a651c52e98afff65911f
reinterpretcat <ilya.builuk@gmail.com>
2019-01-08 23:15:47 +0100

    Add test for objective function

--- src/algorithms/objectives/MaximizeJobAssignment.hpp
+++ src/algorithms/objectives/MaximizeJobAssignment.hpp
@@ -18,20 +18,20 @@ struct maximize_job_assignment final {
     using namespace models::common;
     using namespace ranges;
 
-    auto penalty = static_cast<Cost>(sln.unassigned.size() * Penalty);
-
-    auto total = ranges::accumulate(sln.routes, Cost{penalty}, [&](auto outer, const auto& r) {
+    auto actual = ranges::accumulate(sln.routes, Cost{0}, [&](auto acc, const auto& r) {
       return ranges::accumulate(
         view::concat(view::single(r->start), r->tour.activities(), view::single(r->end)) | view::sliding(2),
-        outer + r->actor->vehicle->costs.fixed + problem.activity->cost(*r.actor, *r.start, r.start->schedule.arrival),
+        acc + r->actor->vehicle->costs.fixed + problem.activity->cost(*r->actor, *r->start, r->start->schedule.arrival),
         [&](auto inner, const auto& view) {
           auto [from, to] = std::tie(*std::begin(view), *(std::begin(view) + 1));
-          return inner + problem.activity->cost(*r.actor, *to, to->schedule.arrival) +
-            problem.transport->cost(*r.actor, *from, *to, from->schedule.departure);
+          return inner + problem.activity->cost(*r->actor, *to, to->schedule.arrival) +
+            problem.transport->cost(*r->actor, from->detail.location, to->detail.location, from->schedule.departure);
         });
     });
 
-    return {total, penalty};
+    auto penalty = static_cast<Cost>(sln.unassigned.size() * Penalty);
+
+    return {actual, penalty};
   }
 };
 }
--- test/algorithms/objectives/MaximizeJobAssignmentTest.cc
+++ test/algorithms/objectives/MaximizeJobAssignmentTest.cc
@@ -1,10 +1,67 @@
 #include "algorithms/objectives/MaximizeJobAssignment.hpp"
 
+#include "test_utils/algorithms/construction/constraints/Helpers.hpp"
+#include "test_utils/fakes/TestTransportCosts.hpp"
+#include "test_utils/models/Factories.hpp"
+
 #include <catch/catch.hpp>
+
+using namespace vrp::algorithms::construction;
+using namespace vrp::algorithms::objectives;
+using namespace vrp::models;
+using namespace vrp::models::costs;
+using namespace vrp::models::problem;
+using namespace vrp::models::solution;
+using namespace vrp::utils;
+
+namespace {
+
+inline auto
+createRoute(const vrp::models::solution::Route::Actor& actor, const vrp::models::common::Schedule& endSchedule) {
+  using namespace vrp::test;
+
+  return test_build_route{}
+    .actor(actor)
+    .start(test_build_activity{}.schedule({0, 0}).type(Activity::Type::Start).shared())
+    .end(test_build_activity{}.schedule(endSchedule).type(Activity::Type::End).shared())
+    .shared();
+}
+}
 
 namespace vrp::test {
 
 SCENARIO("maximize job assignment calculates cost properly", "[algorithms][objectives]") {
-  // TODO
+  auto fleet = Fleet{};
+  fleet.add(test_build_driver{}.owned())
+    .add(test_build_vehicle{}.id("v1").owned())
+    .add(test_build_vehicle{}.id("v2").owned());
+  auto registry = std::make_shared<Registry>(fleet);
+
+  GIVEN("solution with two routes and one unassigned job") {
+    auto actor1 = getActor("v1", fleet);
+    auto actor2 = getActor("v2", fleet);
+    registry->use(*actor1);
+    registry->use(*actor2);
+
+    auto route1 = createRoute(actor1, {40, 40});
+    route1
+      ->tour  //
+      .add(test_build_activity{}.detail({10, 5, DefaultTimeWindow}).shared())
+      .add(test_build_activity{}.detail({15, 5, DefaultTimeWindow}).shared());
+
+    auto route2 = createRoute(actor1, {11, 11});
+    route2->tour.add(test_build_activity{}.detail({5, 1, DefaultTimeWindow}).shared());
+
+    auto problem = Problem{{}, {}, {}, std::make_shared<ActivityCosts>(), std::make_shared<TestTransportCosts>()};
+    auto solution = Solution{registry, {route1, route2}, {{DefaultService, 0}}};
+
+    WHEN("calculates objective costs") {
+      auto [actual, penalty] = maximize_job_assignment<>{}(problem, solution);
+
+      THEN("actual cost is correct") { REQUIRE(actual == 251); }
+
+      THEN("penalty cost is correct") { REQUIRE(penalty == 1000); }
+    }
+  }
 }
 }
