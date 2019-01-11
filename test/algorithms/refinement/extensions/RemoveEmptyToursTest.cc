6599f8efb9bf1588ff8cba71af863711f6e7e6d4
reinterpretcat <ilya.builuk@gmail.com>
2019-01-12 00:30:21 +0100

    Add tests for refinement

--- test/algorithms/refinement/extensions/RemoveEmptyToursTest.cc
+++ test/algorithms/refinement/extensions/RemoveEmptyToursTest.cc
@@ -1,7 +1,6 @@
-#include "algorithms/refinement/extensions/RestoreInsertionContext.hpp"
+#include "algorithms/refinement/extensions/RemoveEmptyTours.hpp"
 
 #include "test_utils/algorithms/construction/constraints/Helpers.hpp"
-#include "test_utils/algorithms/refinement/MatrixRoutes.hpp"
 #include "test_utils/models/Factories.hpp"
 
 #include <catch/catch.hpp>
@@ -15,14 +14,14 @@ using namespace vrp::utils;
 
 namespace vrp::test {
 
-SCENARIO("restore insertion context skips empty routes", "[algorithms][refinement][extensions]") {
+SCENARIO("remove empty tours works", "[algorithms][refinement][extensions]") {
   auto fleet = Fleet{};
   fleet.add(test_build_driver{}.owned())
     .add(test_build_vehicle{}.id("v1").owned())
     .add(test_build_vehicle{}.id("v2").owned());
   auto registry = std::make_shared<Registry>(fleet);
 
-  GIVEN("solution with empty and non empty tours") {
+  GIVEN("empty and non empty routes") {
     auto actor1 = getActor("v1", fleet);
     auto actor2 = getActor("v2", fleet);
     registry->use(*actor1);
@@ -32,22 +31,18 @@ SCENARIO("restore insertion context skips empty routes", "[algorithms][refinemen
     auto route2 = test_build_route{}.actor(actor2).shared();
     route2->tour.add(test_build_activity{}.location(10).shared());
 
-    auto refinementCtx = RefinementContext{generate_matrix_routes{}(1, 1).first,
-                                           std::make_shared<Random>(),
-                                           std::make_shared<std::set<Job, compare_jobs>>(),
-                                           {},
-                                           0};
-    auto solution = Solution{registry, {route1, route2}, {}};
-    WHEN("context is restored") {
-      auto insertionCtx = restore_insertion_context{}.operator()(refinementCtx, solution);
+    auto ctx = InsertionContext{{}, registry, {}, {}, {}, {{route1, {}}, {route2, {}}}, {}};
+
+    WHEN("remove empty tours") {
+      remove_empty_tours{}(ctx);
 
       THEN("only non empty tour is left") {
-        REQUIRE(insertionCtx.routes.size() == 1);
-        REQUIRE(insertionCtx.routes.begin()->first->actor->vehicle->id == "v2");
+        REQUIRE(ctx.routes.size() == 1);
+        REQUIRE(ctx.routes.begin()->first->actor->vehicle->id == "v2");
       }
 
       THEN("empty route's actor is released in registry") {
-        auto actors = insertionCtx.registry->actors() | ranges::to_vector;
+        auto actors = ctx.registry->actors() | ranges::to_vector;
         REQUIRE(actors.size() == 1);
         REQUIRE(actors.front()->vehicle->id == "v1");
       }
