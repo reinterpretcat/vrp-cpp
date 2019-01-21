164df42cd4866e60b250bf126f9f6873d0cb4314
reinterpretcat <ilya.builuk@gmail.com>
2019-01-21 18:48:21 +0100

    Compare activities by pointer

--- src/models/extensions/solution/Comparators.hpp
+++ src/models/extensions/solution/Comparators.hpp
@@ -10,20 +10,11 @@
 
 namespace vrp::models::solution {
 
-/// Compares activities.
-struct compare_activities final {
-  bool operator()(const Activity& lhs, const Activity& rhs) const {
-    // TODO compare jobs as well?
-    return lhs.detail.location == rhs.detail.location ? common::compare_schedules{}(lhs.schedule, rhs.schedule)
-                                                      : lhs.detail.location < rhs.detail.location;
-  }
-};
-
 /// Compares pairs of activity and key.
 struct compare_activities_with_key final {
   bool operator()(const std::pair<Tour::Activity, std::string>& lhs,
                   const std::pair<Tour::Activity, std::string>& rhs) const {
-    return lhs.second == rhs.second ? compare_activities{}(*lhs.first, *rhs.first) : lhs.second < rhs.second;
+    return lhs.second == rhs.second ? lhs.first.get() < rhs.first.get() : lhs.second < rhs.second;
   }
 };
 
