c821846761575fded233d497140e30fc1ab40350
reinterpretcat <ilya.builuk@gmail.com>
2019-01-22 23:20:10 +0100

    Minor refactoring

--- src/utils/extensions/Ranges.hpp
+++ src/utils/extensions/Ranges.hpp
@@ -7,7 +7,7 @@ namespace vrp::utils {
 /// Accumulates values while predicate returns true.
 template<typename View, typename T, typename Pred, typename Accumulator>
 T
-accumulate_while(View view, T value, Pred predicate, Accumulator accumulator) {
+accumulate_while(const View& view, T value, Pred predicate, Accumulator accumulator) {
   for (auto it = ranges::begin(view); it != ranges::end(view); ++it) {
     if (!predicate(value)) break;
     value = accumulator(value, *it);
