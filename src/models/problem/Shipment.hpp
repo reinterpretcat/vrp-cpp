fbc1ed0181c759c68fcd27abd58d526d6549aedd
reinterpretcat <ilya.builuk@gmail.com>
2019-01-25 12:22:00 +0100

    Improve shipmend API

--- src/models/problem/Shipment.hpp
+++ src/models/problem/Shipment.hpp
@@ -1,23 +1,18 @@
 #pragma once
 
-#include "models/common/Dimension.hpp"
+#include "models/problem/Service.hpp"
 
 #include <vector>
 
 namespace vrp::models::problem {
 
-/// Represents a job which consists of two dependent parts: pickup and delivery.
+/// Represents a job which consists of multiple sub jobs without ids.
+/// All of these jobs must be performed in the order specified or none of them.
 struct Shipment final {
-  /// Specifies shipment id.
-  std::string id;
+  /// A list of sub jobs which must be performed in order specified.
+  std::vector<Service> jobs;
 
-  //  /// Pickup service performed before delivery.
-  //  JobDetail pickup;
-  //
-  //  /// Delivery service performed after pickup.
-  //  JobDetail delivery;
-
-  /// Dimensions which simulates work requirements.
+  /// Common shipment dimensions which simulates work requirements.
   vrp::models::common::Dimensions dimens;
 };
 
