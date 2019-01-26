24adabfe28802dd5f306b75dc3ad40e04750ea33
reinterpretcat <ilya.builuk@gmail.com>
2019-01-27 00:09:38 +0100

    Move evaluators into one class

--- src/algorithms/construction/InsertionEvaluator.hpp
+++ src/algorithms/construction/InsertionEvaluator.hpp
@@ -3,16 +3,12 @@
 #include "algorithms/construction/InsertionConstraint.hpp"
 #include "algorithms/construction/InsertionContext.hpp"
 #include "algorithms/construction/InsertionResult.hpp"
-#include "algorithms/construction/evaluators/SequenceInsertionEvaluator.hpp"
-#include "algorithms/construction/evaluators/ServiceInsertionEvaluator.hpp"
 #include "algorithms/construction/extensions/Insertions.hpp"
 #include "models/extensions/problem/Helpers.hpp"
 #include "models/extensions/solution/Factories.hpp"
 #include "models/problem/Fleet.hpp"
 #include "models/solution/Registry.hpp"
 
-#include <chrono>
-#include <iostream>
 #include <numeric>
 #include <utility>
 #include <vector>
@@ -21,7 +17,40 @@ namespace vrp::algorithms::construction {
 
 /// Provides the way to evaluate insertion cost.
 struct InsertionEvaluator final {
+private:
+  /// Defines evaluation context.
+  struct Context final {
+    bool isStopped;    /// True, if processing has to be stopped.
+    int code = 0;      /// Violation code.
+    size_t index = 0;  /// Insertion index.
+    models::common::Cost cost = std::numeric_limits<models::common::Cost>::max();  /// Best cost.
+    models::solution::Activity::Detail detail;                                     /// Activity detail.
 
+    /// Creates a new context.
+    static Context empty(const models::common::Cost& cost) { return {false, 0, 0, cost, {}}; }
+
+    /// Creates a new context from old one when insertion failed.
+    static Context fail(std::tuple<bool, int> error, const Context& other) {
+      return {std::get<0>(error), std::get<1>(error), other.index, other.cost, other.detail};
+    }
+
+    /// Creates a new context from old one when insertion worse.
+    static Context skip(const Context& other) {
+      return {other.isStopped, other.code, other.index, other.cost, other.detail};
+    }
+
+    /// Creates a new context.
+    static Context success(size_t index,
+                           const models::common::Cost& cost,
+                           const models::solution::Activity::Detail& detail) {
+      return {false, 0, index, cost, detail};
+    }
+
+    /// Checks whether insertion is found.
+    bool isSuccess() const { return cost < std::numeric_limits<models::common::Cost>::max(); }
+  };
+
+public:
   /// Evaluates possibility to preform insertion from given insertion context.
   InsertionResult evaluate(const models::problem::Job& job, const InsertionContext& ctx) const {
     using namespace ranges;
@@ -52,10 +81,10 @@ struct InsertionEvaluator final {
         auto result = models::problem::analyze_job<InsertionResult>(
           job,
           [&](const std::shared_ptr<const models::problem::Service>& service) {
-            return ServiceInsertionEvaluator{}.evaluate(job, service, routeCtx, *ctx.constraint, progress);
+            return evaluateService(job, service, routeCtx, *ctx.constraint, progress);
           },
           [&](const std::shared_ptr<const models::problem::Sequence>& sequence) {
-            return SequenceInsertionEvaluator{}.evaluate(job, sequence, routeCtx, *ctx.constraint, progress);
+            return evaluateSequence(job, sequence, routeCtx, *ctx.constraint, progress);
           });
 
         // propagate best result or failure
@@ -86,6 +115,69 @@ private:
                  .shared();
 
     return {start, end};
+  }
+
+  /// Evaluates service insertion.
+  InsertionResult evaluateService(const models::problem::Job& job,
+                                  const std::shared_ptr<const models::problem::Service>& service,
+                                  const InsertionRouteContext& ctx,
+                                  const InsertionConstraint& constraint,
+                                  const InsertionProgress& progress) const {
+    using namespace ranges;
+    using namespace vrp::models;
+    using ActivityType = solution::Activity::Type;
+
+    auto activity = std::make_shared<solution::Activity>(solution::Activity{ActivityType::Job, {}, {}, job});
+    const auto& route = *ctx.route;
+
+    // calculate additional costs on route level.
+    auto routeCosts = constraint.soft(ctx, job);
+
+    // form route legs from a new route view.
+    auto tour = view::concat(view::single(route.start), route.tour.activities(), view::single(route.end));
+    auto legs = view::zip(tour | view::sliding(2), view::iota(static_cast<size_t>(0)));
+    auto evalCtx = Context::empty(progress.bestCost);
+    auto pred = [](const Context& ctx) { return !ctx.isStopped; };
+
+    // 1. analyze route legs
+    auto result = utils::accumulate_while(legs, evalCtx, pred, [&](const auto& out, const auto& view) {
+      auto [items, index] = view;
+      auto [prev, next] = std::tie(*std::begin(items), *(std::begin(items) + 1));
+      auto actCtx = InsertionActivityContext{index, prev, activity, next};
+
+      // 2. analyze service details
+      return utils::accumulate_while(view::all(service->details), out, pred, [&](const auto& in1, const auto& detail) {
+        // TODO check whether tw is empty
+        // 3. analyze detail time windows
+        return utils::accumulate_while(view::all(detail.times), in1, pred, [&](const auto& in2, const auto& time) {
+          activity->detail = {detail.location.value_or(actCtx.prev->detail.location), detail.duration, time};
+          // check hard activity constraint
+          auto status = constraint.hard(ctx, actCtx);
+          if (status.has_value()) return Context::fail(status.value(), in2);
+
+          auto totalCosts = routeCosts + constraint.soft(ctx, actCtx);
+          return totalCosts < in2.cost
+            ? Context::success(actCtx.index, totalCosts, {activity->detail.location, detail.duration, time})
+            : Context::skip(in2);
+        });
+      });
+    });
+
+    activity->detail = result.detail;
+
+    return result.isSuccess()
+      ? make_result_success({result.cost, activity->job.value(), {{activity, result.index}}, ctx})
+      : make_result_failure(result.code);
+  }
+
+  /// Evaluates service insertion.
+  InsertionResult evaluateSequence(const models::problem::Job& job,
+                                   const std::shared_ptr<const models::problem::Sequence>& sequence,
+                                   const InsertionRouteContext& ctx,
+                                   const InsertionConstraint& constraint,
+                                   const InsertionProgress& progress) const {
+    // TODO
+    return make_result_failure(0);
   }
 };
 
--- test/algorithms/construction/InsertionEvaluatorTest.cc
+++ test/algorithms/construction/InsertionEvaluatorTest.cc
@@ -14,12 +14,51 @@ using namespace vrp::models::problem;
 using namespace vrp::models::solution;
 using namespace vrp::algorithms::construction;
 using namespace vrp::test;
+using namespace ranges;
 
 namespace {
 
+std::shared_ptr<Fleet>
+createFleet() {
+  auto fleet = std::make_shared<Fleet>();
+  (*fleet).add(test_build_driver{}.owned()).add(test_build_vehicle{}.id("v1").details({{0, {}, {0, 100}}}).owned());
+  return fleet;
+}
+
 Tour::Activity
-create(const std::string& id, int size) {
+createActivity(const std::string& id, int size) {
   return test_build_activity{}.job(as_job(test_build_service{}.id(id).dimens({{"size", size}}).shared())).shared();
+}
+
+auto
+createContext(const Tour::Activity& prev, const Tour::Activity& next) {
+  using namespace vrp::test;
+
+  auto constraint = std::make_shared<InsertionConstraint>();
+  constraint->add<VehicleActivityTiming>(std::make_shared<VehicleActivityTiming>(
+    createFleet(), std::make_shared<TestTransportCosts>(), std::make_shared<ActivityCosts>()));
+
+  return test_build_insertion_context{}
+    .constraint(constraint)
+    .progress(test_build_insertion_progress{}.owned())
+    .routes({test_build_insertion_route_context{}.add(prev).add(next).owned()})
+    .registry(std::make_shared<Registry>(*createFleet()))
+    .owned();
+}
+
+std::vector<TimeWindow>
+times(std::initializer_list<TimeWindow> tws) {
+  return tws;
+}
+
+std::vector<Location>
+locations(std::initializer_list<Location> locs) {
+  return locs;
+}
+
+std::vector<Service::Detail>
+details(std::initializer_list<Service::Detail> ds) {
+  return ds;
 }
 }
 
@@ -63,16 +102,117 @@ SCENARIO("insertion evaluator can handle service insertion with time constraints
   }
 }
 
+SCENARIO("insertion evaluator can insert service", "[algorithms][construction][insertion]") {
+  GIVEN("empty tour") {
+    auto route = test_build_route{}.owned();
+
+    auto context = test_build_insertion_context{}
+                     .constraint(std::make_shared<InsertionConstraint>())
+                     .progress(test_build_insertion_progress{}.owned())
+                     .routes({test_build_insertion_route_context{}.owned()})
+                     .registry(std::make_shared<Registry>(*createFleet()))
+                     .owned();
+
+    WHEN("service is ok") {
+      auto result = InsertionEvaluator{}.evaluate(DefaultService, context);
+
+      THEN("returns insertion success") {
+        REQUIRE(result.index() == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].second == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].first->detail.location == DefaultJobLocation);
+      }
+    }
+
+    WHEN("service has no location") {
+      auto result = InsertionEvaluator{}.evaluate(
+        as_job(test_build_service{}.details({{{}, DefaultDuration, {DefaultTimeWindow}}}).shared()), context);
+
+      THEN("returns correct insertion success") {
+        REQUIRE(result.index() == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].second == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].first->detail.location == 0);
+      }
+    }
+  }
+
+  GIVEN("tour with two simple activities and service with time windows variations") {
+    auto prev = test_build_activity{}.location(5).schedule({5, 5}).shared();
+    auto next = test_build_activity{}.location(10).schedule({10, 10}).shared();
+    auto context = createContext(prev, next);
+
+    auto [location, tws, index] = GENERATE(std::make_tuple(3, times({DefaultTimeWindow}), 0),
+                                           std::make_tuple(8, times({DefaultTimeWindow}), 1),
+                                           std::make_tuple(7, times({{15, 20}}), 2),
+                                           std::make_tuple(7, times({{15, 20}, {7, 8}}), 1));
+
+    WHEN("service is inserted") {
+      auto service = test_build_service{}.details({{{location}, 0, tws}}).shared();
+      auto result =
+        InsertionEvaluator{}.evaluate(as_job(test_build_service{}.details({{{location}, 0, tws}}).shared()), context);
+
+      THEN("returns correct insertion success") {
+        REQUIRE(result.index() == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].second == index);
+        REQUIRE(ranges::get<0>(result).activities[0].first->detail.location == location);
+      }
+    }
+  }
+
+  GIVEN("tour with two simple activities and different locations") {
+    auto prev = test_build_activity{}.location(5).schedule({5, 5}).shared();
+    auto next = test_build_activity{}.location(10).schedule({10, 10}).shared();
+    auto context = createContext(prev, next);
+
+    auto [locs, index, loc] =
+      GENERATE(std::make_tuple(locations({3}), 0, 3), std::make_tuple(locations({20, 3}), 0, 3));
+
+    WHEN("service is inserted") {
+      auto ds = view::all(locs) | view::transform([&](const auto& l) {
+                  return Service::Detail{{l}, 0, {DefaultTimeWindow}};
+                });
+
+      auto result = InsertionEvaluator{}.evaluate(
+        as_job(test_build_service{}.details(std::vector<Service::Detail>{ds}).shared()), context);
+
+      THEN("returns correct insertion success") {
+        REQUIRE(result.index() == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].second == index);
+        REQUIRE(ranges::get<0>(result).activities[0].first->detail.location == loc);
+      }
+    }
+  }
+
+  GIVEN("tour with two simple activities and different locations, including constraints") {
+    auto prev = test_build_activity{}.location(5).schedule({5, 5}).shared();
+    auto next = test_build_activity{}.location(10).schedule({10, 10}).shared();
+    auto context = createContext(prev, next);
+
+    auto [ds, index, loc] = GENERATE(
+      std::make_tuple(details({{{3}, 0, {DefaultTimeWindow}}}), 0, 3),
+      std::make_tuple(details({{{20}, 0, {DefaultTimeWindow}}, {{3}, 0, times({{0, 2}})}}), 1, 20),
+      std::make_tuple(details({{{12}, 0, {DefaultTimeWindow}}, {{11}, 0, times({DefaultTimeWindow})}}), 1, 11));
+
+    WHEN("service is inserted") {
+      auto result = InsertionEvaluator{}.evaluate(
+        as_job(test_build_service{}.details(std::vector<Service::Detail>{ds}).shared()), context);
+
+      THEN("returns correct insertion success") {
+        REQUIRE(result.index() == 0);
+        REQUIRE(ranges::get<0>(result).activities[0].second == index);
+        REQUIRE(ranges::get<0>(result).activities[0].first->detail.location == loc);
+      }
+    }
+  }
+}
+
 SCENARIO("insertion evaluator can handle service insertion with violation", "[algorithms][construction][insertion]") {
   GIVEN("failed constraint") {
+    auto fleet = createFleet();
     auto constraint = std::make_shared<InsertionConstraint>();
     constraint->addHardRoute([](const auto&, const auto&) { return HardRouteConstraint::Result{42}; });
-    auto evaluator = InsertionEvaluator{};
-    auto fleet = std::make_shared<Fleet>();
-    (*fleet).add(test_build_driver{}.owned()).add(test_build_vehicle{}.id("v1").details({{0, {}, {0, 100}}}).owned());
 
     WHEN("service is evaluated") {
-      auto result = evaluator.evaluate(
+      auto result = InsertionEvaluator{}.evaluate(
         DefaultService,
         test_build_insertion_context{}.constraint(constraint).registry(std::make_shared<Registry>(*fleet)).owned());
 
