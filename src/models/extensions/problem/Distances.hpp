3f036ce63956aba6206b284a1d58ad6a77ad02c3
reinterpretcat <ilya.builuk@gmail.com>
2019-02-03 00:02:42 +0100

    Support sequence in job distance

--- src/models/extensions/problem/Distances.hpp
+++ src/models/extensions/problem/Distances.hpp
@@ -16,29 +16,32 @@ struct job_distance final {
 
   common::Distance operator()(const Job& lhs, const Job& rhs) const {
     using namespace ranges;
+
     auto left = getLocations(lhs) | to_vector;
     auto right = getLocations(rhs) | to_vector;
 
-    auto distance =
-      ranges::min(view::concat(view::cartesian_product(left, right) | view::transform([&](const auto& tuple) {
-                                 return transport.distance(profile, std::get<0>(tuple), std::get<1>(tuple), departure);
-                               }),
-                               view::single(common::NoDistance)));
-
-    return distance < common::NoDistance ? distance : 0;
+    return ranges::min(view::concat(view::cartesian_product(left, right) | view::transform([&](const auto& tuple) {
+                                      auto lhs = std::get<0>(tuple);
+                                      auto rhs = std::get<1>(tuple);
+                                      return lhs.has_value() && rhs.has_value()
+                                        ? transport.distance(profile, lhs.value(), rhs.value(), departure)
+                                        : 0;
+                                    })));
   }
 
 private:
-  ranges::any_view<common::Location> getLocations(const Job& job) const {
-    return analyze_job<ranges::any_view<common::Location>>(
+  using LocationView = ranges::any_view<std::optional<common::Location>>;
+
+  LocationView getLocations(const Job& job) const {
+    return analyze_job<LocationView>(
       job,
-      [](const std::shared_ptr<const Service>& service) -> ranges::any_view<common::Location> {
-        return ranges::view::for_each(service->details, [](const auto& d) {
-          return ranges::yield_if(d.location.has_value(), d.location.has_value() ? d.location.value() : 0);
-        });
+      [](const std::shared_ptr<const Service>& service) -> LocationView {
+        return ranges::view::for_each(service->details, [](const auto& d) { return ranges::yield(d.location); });
       },
-      [](const std::shared_ptr<const Sequence>& sequence) -> ranges::any_view<common::Location> {
-        throw std::domain_error("not implemented");
+      [](const std::shared_ptr<const Sequence>& sequence) -> LocationView {
+        return ranges::view::for_each(sequence->jobs, [](const auto& service) {
+          return ranges::view::for_each(service.details, [](const auto& d) { return ranges::yield(d.location); });
+        });
       });
   }
 };
--- test/models/extensions/problem/DistancesTest.cc
+++ test/models/extensions/problem/DistancesTest.cc
@@ -23,16 +23,46 @@ jobDetail(const std::optional<Location>& location) {
 namespace vrp::test {
 
 SCENARIO("distance between service jobs", "[models][extensions][problem]") {
-  auto [d1, d2, expected] = GENERATE(table<JobDetails, JobDetails, Distance>(
-    {{{jobDetail({0})}, {jobDetail({10})}, 10},
-     {{jobDetail({0})}, {jobDetail({})}, 0},
-     {{jobDetail({})}, {jobDetail({})}, 0},
-     {{jobDetail({3})}, {jobDetail({5}), jobDetail({2})}, 1},
-     {{jobDetail({2}), jobDetail({1})}, {jobDetail({10}), jobDetail({9})}, 7}}));
+  auto [d1, d2, expected] = GENERATE(table<JobDetails, JobDetails, Distance>({
+    {{jobDetail({0})}, {jobDetail({10})}, 10},
+    {{jobDetail({0})}, {jobDetail({})}, 0},
+    {{jobDetail({})}, {jobDetail({})}, 0},
+    {{jobDetail({3})}, {jobDetail({5}), jobDetail({2})}, 1},
+    {{jobDetail({2}), jobDetail({1})}, {jobDetail({10}), jobDetail({9})}, 7},  //
+  }));
 
   GIVEN("service jobs") {
     auto s1 = as_job(test_build_service{}.details(std::move(d1)).shared());
     auto s2 = as_job(test_build_service{}.details(std::move(d2)).shared());
+
+    WHEN("distance calculated") {
+      auto result = job_distance{TestTransportCosts{}, "", Timestamp{}}.operator()(s1, s2);
+
+      THEN("has expected value") { REQUIRE(result == expected); }
+    }
+  }
+}
+
+
+SCENARIO("distance between sequence jobs", "[models][extensions][problem]") {
+  auto [d11, d12, d21, d22, expected] = GENERATE(table<JobDetails, JobDetails, JobDetails, JobDetails, Distance>({
+    {{jobDetail({1})}, {jobDetail({2})}, {jobDetail({8})}, {jobDetail({9})}, 6},
+    {{jobDetail({1})}, {jobDetail({2})}, {jobDetail({})}, {jobDetail({9})}, 0},
+    {{jobDetail({})}, {jobDetail({})}, {jobDetail({})}, {jobDetail({9})}, 0},
+    {{jobDetail({})}, {jobDetail({})}, {jobDetail({})}, {jobDetail({})}, 0},  //
+  }));
+
+  GIVEN("service jobs") {
+    auto s1 = as_job(test_build_sequence{}
+                       .id("seq1")
+                       .service(test_build_service{}.id("s1").details(std::move(d11)).owned())
+                       .service(test_build_service{}.id("s2").details(std::move(d12)).owned())
+                       .shared());
+    auto s2 = as_job(test_build_sequence{}
+                       .id("seq2")
+                       .service(test_build_service{}.id("s1").details(std::move(d21)).owned())
+                       .service(test_build_service{}.id("s2").details(std::move(d22)).owned())
+                       .shared());
 
     WHEN("distance calculated") {
       auto result = job_distance{TestTransportCosts{}, "", Timestamp{}}.operator()(s1, s2);
