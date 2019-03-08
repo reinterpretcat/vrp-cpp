a7f21ba2215a2c14ac69a56cd407c01ca62da207
reinterpretcat <ilya.builuk@gmail.com>
2019-03-08 23:09:07 +0100

    Add actor job lock constraint

---
+++ src/algorithms/construction/constraints/ActorJobLock.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "algorithms/construction/InsertionConstraint.hpp"
+#include "models/extensions/problem/Comparators.hpp"
+
+namespace vrp::algorithms::construction {
+
+/// Allows to lock specific actors within specific jobs.
+struct ActorJobLock final : public HardRouteConstraint {
+  explicit ActorJobLock(int code = 3) : code_(code), locks_() {}
+
+  ranges::any_view<int> stateKeys() const override { return ranges::view::empty<int>(); }
+
+  void accept(InsertionRouteContext& context) const override {}
+
+  /// Locks actor within job.
+  ActorJobLock& lock(const std::shared_ptr<const models::solution::Actor>& actor, const models::problem::Job& job) {
+    locks_[job].insert(actor);
+
+    return *this;
+  }
+
+  HardRouteConstraint::Result hard(const InsertionRouteContext& routeCtx,
+                                   const HardRouteConstraint::Job& job) const override {
+    auto jobLocks = locks_.find(job);
+
+    if (jobLocks != locks_.end() && jobLocks->second.find(routeCtx.route->actor) == jobLocks->second.end())
+      return HardRouteConstraint::Result{3};
+
+    return {};
+  }
+
+private:
+  int code_;
+  std::map<models::problem::Job,                                      //
+           std::set<std::shared_ptr<const models::solution::Actor>>,  //
+           models::problem::compare_jobs>
+    locks_;
+};
+}
---
+++ test/algorithms/construction/constraints/ActorJobLockTest.cc
@@ -0,0 +1,57 @@
+#include "algorithms/construction/constraints/ActorJobLock.hpp"
+
+#include "models/problem/Fleet.hpp"
+#include "models/solution/Registry.hpp"
+#include "test_utils/algorithms/construction/constraints/Helpers.hpp"
+#include "test_utils/models/Factories.hpp"
+
+#include <catch/catch.hpp>
+
+using namespace vrp::algorithms::construction;
+using namespace vrp::models::problem;
+using namespace vrp::models::solution;
+using namespace ranges;
+
+namespace {
+const Registry::SharedActor
+getActorFromRegistry(const std::string& vehicleId, const Registry& registry) {
+  return ranges::front(registry.available() |
+                       view::remove_if([&](const auto& a) { return get_vehicle_id{}(*a->vehicle) != vehicleId; }) |
+                       to_vector);
+}
+}
+
+namespace vrp::test {
+
+SCENARIO("actor job lock can manage actor-job locks", "[algorithms][construction][constraints]") {
+  auto [locked, used, expected] = GENERATE(table<std::string, std::string, HardRouteConstraint::Result>({
+    {"v1", "v1", {}},
+    {"v1", "v2", {3}},
+  }));
+
+  GIVEN("fleet with 2 vehicles") {
+    auto fleet = std::make_shared<Fleet>();
+    (*fleet)  //
+      .add(test_build_driver{}.owned())
+      .add(test_build_vehicle{}.id("v1").details(asDetails(0, {}, {0, 100})).owned())
+      .add(test_build_vehicle{}.id("v2").details(asDetails(0, {}, {0, 100})).owned());
+    auto registry = Registry(*fleet);
+
+    WHEN("has job lock for one actor") {
+      auto actorJobLock = ActorJobLock{};
+      actorJobLock.lock(getActorFromRegistry(locked, registry), DefaultService);
+
+      THEN("returns expected constraint check") {
+        auto result = actorJobLock.hard(
+          InsertionRouteContext{std::make_shared<Route>(Route{getActorFromRegistry(used, registry), {}}),
+                                std::make_shared<InsertionRouteState>()},
+          DefaultService);
+
+        REQUIRE(result == expected);
+      }
+    }
+  }
+}
+
+// TODO
+}
