9ea983468f7645b09790fa3df9229481c4fdb442
reinterpretcat <ilya.builuk@gmail.com>
2019-03-31 11:05:16 +0200

    Rename HereProblemJson to HereSolutionJson

--- test/streams/out/json/HereSolutionJsonTest.cc
+++ test/streams/out/json/HereSolutionJsonTest.cc
@@ -1,8 +1,7 @@
-#include "streams/out/json/HereProblemJson.hpp"
-
 #include "Solver.hpp"
 #include "algorithms/refinement/logging/LogToConsole.hpp"
 #include "streams/in/json/HereJson.hpp"
+#include "streams/out/json/HereSolutionJson.hpp"
 #include "test_utils/streams/HereModelBuilders.hpp"
 
 #include <catch/catch.hpp>
