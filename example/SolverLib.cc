67ad3749fd3453af7a9339f464067ecff072eff3
reinterpretcat <ilya.builuk@gmail.com>
2019-04-08 12:09:37 +0200

    Improve solver lib

--- example/SolverLib.cc
+++ example/SolverLib.cc
@@ -7,6 +7,8 @@
 #endif
 
 #include "Solver.hpp"
+#include "streams/in/json/HereProblemJson.hpp"
+#include "streams/out/json/HereSolutionJson.hpp"
 
 #include <sstream>
 #include <string>
@@ -17,20 +19,30 @@ typedef void OnSuccess(const char* solution);
 /// Called when solution cannot be created.
 typedef void OnError(const char* error);
 
-/// Executes function and catches exception if it occurs.
-inline void
-safeExecute(const std::function<void()>& action, OnError* onError) {
-  try {
-    action();
-  } catch (std::exception& ex) { onError(ex.what()); }
-}
-
 extern "C" {
 void EXPORT_API
-solve(const char* problem, const char* format, OnSuccess* onSuccess, OnError* onError) {
+solve(const char* jsonProblem, const char* format, OnSuccess* onSuccess, OnError* onError) {
+  // TODO extract this functionality to header and reuse it in SolverExe
+  auto solver = vrp::Solver<vrp::algorithms::refinement::create_refinement_context<>,
+                            vrp::algorithms::refinement::select_best_solution,
+                            vrp::algorithms::refinement::ruin_and_recreate_solution<>,
+                            vrp::algorithms::refinement::GreedyAcceptance<>,
+                            vrp::algorithms::refinement::MaxIterationCriteria,
+                            vrp::algorithms::refinement::log_to_nothing>{};
+
   if (std::strcmp(format, "here") == 0) {
-    // TODO
-    onSuccess("Some result: TODO");
+    std::stringstream ss;
+    ss << jsonProblem;
+
+    auto problem = vrp::streams::in::read_here_json_type{}(ss);
+    auto solution = solver(problem);
+
+    ss.str("");
+    ss.clear();
+
+    vrp::streams::out::dump_solution_as_here_json{problem}(ss, solution);
+    auto jsonSolution = ss.str();
+    onSuccess(jsonSolution.data());
   } else {
     // TODO add all supported inputs
     onError("Not supported");
