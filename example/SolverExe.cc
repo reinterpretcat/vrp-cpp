c513c16fbfc93a27d1be89afee4ce5a3ee416b1a
reinterpretcat <ilya.builuk@gmail.com>
2019-04-09 22:05:58 +0200

    Add writing solution into file

--- example/SolverExe.cc
+++ example/SolverExe.cc
@@ -9,10 +9,17 @@ using namespace vrp::streams::in;
 
 int
 main(int argc, char* argv[]) {
-  if (argc < 4) throw std::invalid_argument("Usage:\n\t$solver Path inType outType");
+  if (argc < 4) throw std::invalid_argument("Usage:\n\t$solver Path inType outType <outPath>");
 
   auto inStream = std::fstream(argv[1], std::ios::in);
-  vrp::example::solve_based_on_type{}(argv[2], inStream, argv[3], std::cout);
+
+  auto solver = vrp::example::solve_based_on_type{};
+  if (argc > 4) {
+    auto outStream = std::fstream(argv[4], std::ios::out);
+    solver(argv[2], inStream, argv[3], outStream);
+  } else {
+    solver(argv[2], inStream, argv[3], std::cout);
+  }
 
   return 0;
 }
